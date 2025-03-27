f6aaee5a27c341df51c8172a4bceadef508b02ba
Alice Mugure <machariamugure17@gmail.com>
2025-03-27 19:53:22 +0300

    First Fetch

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    const baseUrl = 'http://localhost:3001/products'
+    const baseUrl = 'http://localhost:3001/products';
     const shopNowBtn = document.querySelector('.shop');
     const makeup = document.querySelector('.makeup-products');
     const jewelry = document.querySelector('.jewelry-products');
     const clothing = document.querySelector('.clothing-products');
     const companyName = document.querySelector('.CompanyName');
     const body = document.body;
-
+    const productsContainer = document.querySelector('#products'); 
 
     fetch(baseUrl)
-        .then(response => response.json())
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(res);
+            }
+            return response.json();
+        })
         .then(data => {
             const products = data.products;
             const categories = data.categories;
 
             shopNowBtn.addEventListener('click', function(e) {
                 e.preventDefault();
-                document.querySelector('#products')
+                displayProducts(products); 
             });
         })
-})
+        .catch(err => {
+            return (err);
+        });
+});
+
+
