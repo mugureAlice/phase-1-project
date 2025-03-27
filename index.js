7ed9c9f6238ab6256b32bb21b0774c70783fa32c
Alice Mugure <machariamugure17@gmail.com>
2025-03-27 22:58:02 +0300

    removed unwanted images

--- index.js
+++ index.js
@@ -1,78 +1,52 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const baseUrl = 'http://localhost:3001/products';
-    const shopNowBtn = document.querySelector('.shop');
-    const productsContainer = document.querySelector('#products'); 
-    const products = [];
-
-    fetch(baseUrl)
-        .then(res => res.json())
-        .then(data => {
-            products = data;
-        })
-    .catch((err) => console.log(err));
-
-function displayProducts(products) { 
-     productsContainer.innerHTML = ''; 
-
-    products.forEach((product) => {
-        const productElement = document.createElement('div');
-        productElement.className = 'single-product';
-        productElement.innerHTML = `
-            <h5>${product.name}</h5>
-            <img src="${product.image}" alt="${product.name}">
-            <p>${product.description}</p>
-            <div>${product.category}</div>
-            <div>${product.price}</div>
-            <div id="buttons">
-                <button class="shop-category">Shop Now</button>
-            </div>
-        `;
-        productsContainer.appendChild(productElement);
+document.addEventListener("DOMContentLoaded", function () {
+    const baseUrl = "http://localhost:3001/products";
     
-    });
+    const shopNowBtn = document.querySelector(".shop");
+    const productsContainer = document.querySelector("#products");
+    let products = [];
+
+    
+    fetch(baseUrl)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error("Failed to fetch products");
+            }
+            return res.json();
+        })
+        .then((data) => {
+            products = data;
+            displayProducts(products);
+        })
+        .catch((err) => console.error(err));
 
 
+    function displayProducts(products) {
+        productsContainer.innerHTML = ""; 
 
-document.querySelectorAll(".shop-category")
-shop-category.forEach((button) => {
-    button.addEventListener("click", () => {
-        alert("Item added to cart!");
-    });
+        products.forEach((product) => {
+            const productElement = document.createElement("div");
+            productElement.className = "single-product";
+
+            productElement.innerHTML = `
+                <h5>${product.name}</h5>
+                <img src="${product.image}" alt="${product.name}">
+                <p>${product.description}</p>
+                <div>${product.category}</div>
+                <div>Price: $${product.price}</div>
+                <button class="shop-category">Shop Now</button>
+            `;
+
+            productsContainer.appendChild(productElement);
+        });
+
+       const shop_category = document.querySelectorAll(".shop-category")
+       shop_category .forEach((button) => {
+            button.addEventListener("click", () => {
+                alert("Item added to cart!");
+            });
+        });
+    }
+
+    
 });
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
